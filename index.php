dba416d8a90b5d0026a5b12e5e0b664d9069a5ea
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-11-09 13:37:34 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php require_once('header.php');?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Javascript</title>
+    <title>Ice Scream Company</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
 
  
 
-    <h1>It is time to use java libraries!</h1>
-   <img src="https://enosburghlibrary.files.wordpress.com/2016/02/coffee-and-books.jpg" alt="Coffee Image" style="height:500px;" /> <!-- image pointer-->
+    <h1>Welcome to The Ice Scream Company</h1>
+   <img src="https://linkprodstorage.blob.core.windows.net/listing-235e1a0b-fbe7-e611-80f4-3863bb2e15c0/shutterstock423134989.jpg" alt="Ice Scream Image" style="height:500px;" /> <!-- image pointer-->
     
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
