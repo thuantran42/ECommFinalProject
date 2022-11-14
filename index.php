a0d49371d2fdc4c1999fc9ad91d742498ff35ed9
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-11-14 13:30:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php require_once('header.php');?>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Ice Scream Company</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Ice Scream Company</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
-  <body>
+<script>
 
- 
+</script>
+<body>
 
-    <h1>Welcome to The Ice Scream Company</h1>
-   <img src="https://linkprodstorage.blob.core.windows.net/listing-235e1a0b-fbe7-e611-80f4-3863bb2e15c0/shutterstock423134989.jpg" alt="Ice Scream Image" style="height:500px;" /> <!-- image pointer-->
-    
-    
+    <div class = "text-center fw-bold text-uppercase">
+        <h1>Welcome to The Ice Scream Company</h1>
+        <img src="https://linkprodstorage.blob.core.windows.net/listing-235e1a0b-fbe7-e611-80f4-3863bb2e15c0/shutterstock423134989.jpg" alt="Ice Scream Image" style="height:500px;" /> <!-- image pointer-->
+    </div>
+
+  
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
