b66be1acdb47d50af160079cf64d70b4214bd035
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-11-21 17:01:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,44 +10,49 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
 <script>
-
+    body
+    {
+        background - image: url("https://img.freepik.com/free-vector/pink-topping-with-sprinkles-background_23-2148639893.jpg?w=1060&t=st=1669070048~exp=1669070648~hmac=19bae5acf5d7e587f01bea58668133e22c03466bd3906ef3b5585e03de79b6e7");
+    }
 </script>
 <body>
 
-    <div img src="https://img.freepik.com/free-vector/pink-topping-with-sprinkles-background_23-2148639893.jpg?w=1060&t=st=1669070048~exp=1669070648~hmac=19bae5acf5d7e587f01bea58668133e22c03466bd3906ef3b5585e03de79b6e7" alt="Ice scream background" style="width:1000px;"/> <!-- One whole main div for general format-->
-    
+    <div> <!-- One whole main div for general format-->
+    <!--<img src="https://img.freepik.com/free-vector/pink-topping-with-sprinkles-background_23-2148639893.jpg?w=1060&t=st=1669070048~exp=1669070648~hmac=19bae5acf5d7e587f01bea58668133e22c03466bd3906ef3b5585e03de79b6e7" alt="Ice scream background" style="width:1000px;" />-->
 
-        <div class="text-center fw-bold text-uppercase"> <!-- Title with Picture-->
-            <h1>Welcome to The Ice Scream Company</h1>
-            
-        </div>
+    <div class="text-center fw-bold text-uppercase">
+        <!-- Title with Picture-->
+        <h1>Welcome to The Ice Scream Company</h1>
 
-
-        <div style="width:1000px; margin-left:auto;margin-right:auto;">
-
-            <img src="https://lh6.googleusercontent.com/Rky71NXiIOF9NrJlzJcaSs9sp3A0j3_eRz3jkbS4hnr3ry7ArbSbyIzOGFiltXTvmpPD2YFRh_viT2mZKyvaVFnRdZddYRahbOlcQrWRhMuqa_xPVdwVsxjK4OBo8Ft_lTU7QBkz7gcj1VSLmloS9S-FYVHFuTjtmlP6ovRgsu3ovHHFE_Aje7lz8Zx8Ug" alt="Ice Scream Image" style="height:500px;" /> <!-- image pointer-->
-        </div>
+    </div>
 
 
-        <div class="text-center fw-bold text-uppercase"> <!-- catch phrase-->
-            <span>I scream! </span> <br />
-            <span>You scream! </span> <br />
-            <span>Give me that, Give me that Ice Scream! </span> <br />
-           
+    <div style="width:1000px; margin-left:auto;margin-right:auto;">
 
-        </div>
+        <img src="https://lh6.googleusercontent.com/Rky71NXiIOF9NrJlzJcaSs9sp3A0j3_eRz3jkbS4hnr3ry7ArbSbyIzOGFiltXTvmpPD2YFRh_viT2mZKyvaVFnRdZddYRahbOlcQrWRhMuqa_xPVdwVsxjK4OBo8Ft_lTU7QBkz7gcj1VSLmloS9S-FYVHFuTjtmlP6ovRgsu3ovHHFE_Aje7lz8Zx8Ug" alt="Ice Scream Image" style="height:500px;" /> <!-- image pointer-->
+    </div>
 
-        <div>
-            <hr />
-            Footer area <br />
-            Copyright reserved by Trae Tran
 
-        </div>
+    <div class="text-center fw-bold text-uppercase">
+        <!-- catch phrase-->
+        <span>I scream! </span> <br />
+        <span>You scream! </span> <br />
+        <span>Give me that, Give me that Ice Scream! </span> <br />
 
 
     </div>
 
-  
+    <div>
+        <hr />
+        Footer area <br />
+        Copyright reserved by Trae Tran
+
+    </div>
+
+
+    </div>
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
