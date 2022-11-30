61846a1421a1fca48f53450918ce500c2be5813c
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-11-29 21:47:59 -0600

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -44,122 +44,158 @@
         </div>-->
     <br /> <!-- Space break -->
 
-        <div> <!-- First Big Div -->
+    <div>
+        <!-- First Big Div -->
+        <div class="card">
+            <div class="card-header">
+                <img src="https://www.sciencenews.org/wp-content/uploads/2021/04/041221_AD_scream_feat_REV.jpg" class="card-img-top" alt="picture of screaming" style="height:100px; width:100px;">
+
+                <span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">About Us</span>       <!-- about us -->
+
+            </div>
+
+            <div class="card-body">
+                <p>
+                    Ice Scream was started in Oklahoma City, Oklahoma and it is founded by Trae Tran.
+                    Trae Tran saw great ambition in creativity from low-end to high-end ice cream in hopes
+                    to appeal to all ice cream lovers. The idea of the customer is always right has been
+                    fundamentally integrated into our creative ice creaming making system.
+                </p>
+
+            </div>
+
+
+
+        </div>
+
+        <br /> <!-- Space Break -->
+
+        <div>
             <div class="card">
                 <div class="card-header">
-                    <img src="https://www.sciencenews.org/wp-content/uploads/2021/04/041221_AD_scream_feat_REV.jpg" class="card-img-top" alt="picture of Trae" style="height:100px; width:100px;">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvsDoSlKbb2vejA8Cfh8jMMvaFSkackvBYH52lFsJIa7Lm6K1hZTqo_1E9b4rYVG0sU1U&usqp=CAU" class="card-img-top" alt="picture of people" style="height:100px; width:100px;">
 
-                    <span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">About Us</span>       <!-- about us -->
+                    <span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">The Mission Statement</span> <!-- Mission statement -->
 
                 </div>
 
                 <div class="card-body">
                     <p>
-                        Ice Scream was started in Oklahoma City, Oklahoma and it is founded by Trae Tran.
-                        Trae Tran saw great ambition in creativity from low-end to high-end ice cream in hopes
-                        to appeal to all ice cream lovers. The idea of the customer is always right has been
-                        fundamentally integrated into our creative ice creaming making system.
+                        The Ice Scream
+                        Company's mission is to inspire one person, one scoop and one place at a time.
                     </p>
 
                 </div>
 
-
-
             </div>
 
-            <br /> <!-- Space Break -->
-
-            <div>
-                <div class="card">
-                    <div class="card-header">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvsDoSlKbb2vejA8Cfh8jMMvaFSkackvBYH52lFsJIa7Lm6K1hZTqo_1E9b4rYVG0sU1U&usqp=CAU" class="card-img-top" alt="picture of Trae" style="height:100px; width:100px;">
-
-                        <span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">The Mission Statement</span> <!-- Mission statement -->
-
-                    </div>
-
-                    <div class="card-body">
-                        <p>
-                            The Ice Scream
-                            Company's mission is to inspire one person, one scoop and one place at a time.
-                        </p>
-
-                    </div>
-
-                </div>
-
-            </div>
+        </div>
 
         <br /> <!-- Space break-->
 
-            <div>
-                <div class="card">
-                    <div class="card-header">
-                        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQH2lw-G4ZBFwg/profile-displayphoto-shrink_800_800/0/1592175896484?e=1675296000&v=beta&t=DmxAUUSIkdgI3VGwnwCstyY99Jgw6xvzK8166MlU8jw" class="card-img-top" alt="picture of Trae" style="height:100px; width:100px;">
-                        <span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">Something Icy about the Founder</span> <!-- Founder Bio/Auto -->
+        <div>
+            <div class="card">
+                <div class="card-header">
+                    <img src="https://media-exp1.licdn.com/dms/image/C4E03AQH2lw-G4ZBFwg/profile-displayphoto-shrink_800_800/0/1592175896484?e=1675296000&v=beta&t=DmxAUUSIkdgI3VGwnwCstyY99Jgw6xvzK8166MlU8jw" class="card-img-top" alt="picture of Trae" style="height:100px; width:100px;">
+                    <span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">Something Icy about the Founder</span> <!-- Founder Bio/Auto -->
 
-                    </div>
+                </div>
 
-                    <div class="card-body">
-                        <p>
-                            The founder, Trae, is committed to making the Ice Scream Company one of the most flexible customer choices for icy snacks around the world. <br />
-                            Trae loves to play basketball and bowling for fun. He has got his Bachelor's in MIS from the University of Oklahoma. (Go sooners!) <br />
-                            Trae had always dreamed of making an ice scream shop to spread the joy to everyone and everyone. 
-                            Here is some words from the Founder of the Ice Scream Company! <br />
-                            "It is my pleasure to present myself on this earth full of ice cream lovers. My goal is to create an exciting environment for all those who <br />
-                            enjoy a great icy snack on any day. I will put forth my effort to creating the best experiences for customers and our associates! I dream that <br />
-                            one day that the world will be screaming Ice Scream!"
+                <div class="card-body">
+                    <p>
+                        The founder, Trae, is committed to making the Ice Scream Company one of the most flexible customer choices for icy snacks around the world. <br />
+                        Trae loves to play basketball and bowling for fun. He has got his Bachelor's in MIS from the University of Oklahoma. (Go sooners!) <br />
+                        Trae had always dreamed of making an ice scream shop that can bring all ice cream lovers alike together! <br />
+                        <br />
+                        Here is some words from the Founder of the Ice Scream Company! <br />
+                        "It is my pleasure to present myself on this earth full of ice cream lovers. My goal is to create an exciting environment for all those who <br />
+                        enjoy a great icy snack on any day. I will put forth my effort to creating the best experiences for customers and our associates! I dream that <br />
+                        one day that the world will be screaming Ice Scream!"
 
-                        </p>
-
-                    </div>
+                    </p>
 
                 </div>
 
             </div>
 
+        </div>
+
+        <br /> <!-- Space Break-->
 
 
+        <div class="card">
+            <div class="card-header">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI3oqhrzMg-Vq_3V5kin_8b9j-bKdX8sPmSg&usqp=CAU" class="card-img-top" alt="Handshake image" style="height:100px; width:100px;">
 
-
-            <div class="fw-bold ">
-                <!-- footer divs -->
-                <hr />
-                <div>
-                    <!-- first row divs in footer-->
-                    <div style="width: 400px; float: left;">
-                        Privacy Policy
-                    </div>
-
-                    <div style="width:400px; float:left;">
-                        Terms & Conditions
-                    </div>
-
-                    <div style="width: 400px; float: left;">
-                        Do Not Sell or Share My Personal Information
-                    </div>
-
-                </div>
-
-
-
-                <div>
-                    <div style="width: 400px; float: left;">
-                        Cookie Settings
-                    </div>
-
-                    <div style="width: 400px; float: left;">
-                        @2018 - 2022 The Ice Scream Comapny Inc.
-                    </div>
-
-                </div>
-
-
+                <span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">Stay Connected With Us</span>       <!-- about us -->
 
             </div>
+
+            <div class="card-body">
+                <p>
+                    Looking to stay connected with us? <br />
+                    Want to hear the latest events happening around the world? <br />
+                    Excited to hear more about new toppings we'll add to the menu? <br />
+                    Looking for an exciting job that'll make you scream? <br />
+                    <br />
+                    Follow us on Linkedin, Instagram, Youtube, and Twitter! <br />
+
+                    <br />
+                    <a href="https://www.linkedin.com/"> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgd4JdyuHOYjmtScEwT2PirsDOyS-6XTAIoA&usqp=CAU" alt="Linkedin link" /> </a> <br />
+                    <a href="https://www.instagram.com/"> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTibNm4alTFbffyXvLcZgMEsrIqnyK66LeszQ&usqp=CAU" alt="Instgram link" /> </a> <br />
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQivdaPpv3KvpL8RUJFo-GLIO6LciuTLhdvVw&usqp=CAU" alt="Youtube link" /> </a> <br />
+                    <a href="https://twitter.com/"> <img src="https://www.pngkey.com/png/full/2-27646_twitter-logo-png-transparent-background-logo-twitter-png.png" alt="Twitter link" /> </a> <br />
+                    
+                </p>
+
+            </div>
+
 
 
         </div>
+
+
+        <br /> <!-- Space Break-->
+
+
+        <div class="fw-bold ">
+            <!-- footer divs -->
+            <hr />
+            <div>
+                <!-- first row divs in footer-->
+                <div style="width: 400px; float: left;">
+                    Privacy Policy
+                </div>
+
+                <div style="width:400px; float:left;">
+                    Terms & Conditions
+                </div>
+
+                <div style="width: 400px; float: left;">
+                    Do Not Sell or Share My Personal Information
+                </div>
+
+            </div>
+
+
+
+            <div>
+                <div style="width: 400px; float: left;">
+                    Cookie Settings
+                </div>
+
+                <div style="width: 400px; float: left;">
+                    @2018 - 2022 The Ice Scream Comapny Inc.
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+
+    </div>
 
 
 
