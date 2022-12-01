b57a3f7dfe50e949c4aec1cfd8328360469d0d58
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-11-30 20:41:48 -0600

    Update customer.php

--- customer.php
+++ customer.php
@@ -68,148 +68,174 @@
     }
     ?>
 
-    <h1> Customer info</h1>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Phone Number</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
 
-            <?php
-            $sql = "SELECT customer_id, customerName, customerPhone from Customer";
-            $result = $conn->query($sql);
+    <div class="card">
+        <div class="card-header">
+            <h1> <span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">Customer info</span></h1> <!-- Customer Info Title-->
+        </div>
+    </div>
 
-            if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-            ?>
 
-            <tr>
-                <td><?=$row["customer_id"]?></td>
-                <td><?=$row["customerName"]?></td>
-                <td><?=$row["customerPhone"]?></td>
-                <td>
-                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editWeapons<?=$row['customer_id']?>">
-                        Edit
-                    </button>
-                    <div class="modal fade" id="editWeapons<?=$row['customer_id']?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editWeapons<?=$row['customer_id']?>Label" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="editWeapons<?=$row['customer_id']?>Label">Edit Customer Information</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <form method="post" action="">
-                                        <div class="mb-3">
-                                            <label for="editWeapons<?=$row['customer_id']?>Name" class="form-label">Name</label>
-                                            <input type="text" class="form-control" id="editWeapons<?=$row['customer_id']?>Name" aria-describedby="editWeapons<?=$row['customer_id']?>Help" name="iName" value="<?=$row['customerName']?>"> <!-- customerName -->
-                                            Phone Number
-                                            <input type="text" class="form-control" id="editWeapons<?=$row['customer_id']?>Name" aria-describedby="editWeapons<?=$row['customer_id']?>Help" name="iPhone" value="<?=$row['customerPhone']?>"> <!-- customerPhone -->
-                                            <div id="editWeapons<?=$row['customer_id']?>Help" class="form-text">Enter the customer's name.</div>
+    
+        <!--<div class="card-header">
+
+        </div>
+        <div class="card-body">
+
+        </div>-->
+    
+
+
+
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th><span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">ID</span></th> <!-- ID Attribute -->
+                            <th><span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">Name</span></th>  <!-- Name Attribute -->
+                            <th><span onmouseover="style.color='blue'" onmouseout="style.color='pink'" style="color: pink">Phone Number</span></th>  <!-- Phone Number Attribute -->
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <?php
+                        $sql = "SELECT customer_id, customerName, customerPhone from Customer";
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0) {
+                        // output data of each row
+                        while($row = $result->fetch_assoc()) {
+                        ?>
+
+                        <tr>
+                            <td><?=$row["customer_id"]?></td>
+                            <td><?=$row["customerName"]?></td>
+                            <td><?=$row["customerPhone"]?></td>
+                            <td>
+                                <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editWeapons<?=$row['customer_id']?>">
+                                    Edit
+                                </button>
+                                <div class="modal fade" id="editWeapons<?=$row['customer_id']?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editWeapons<?=$row['customer_id']?>Label" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="editWeapons<?=$row['customer_id']?>Label">Edit Customer Information</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form method="post" action="">
+                                                    <div class="mb-3">
+                                                        <label for="editWeapons<?=$row['customer_id']?>Name" class="form-label">Name</label>
+                                                        <input type="text" class="form-control" id="editWeapons<?=$row['customer_id']?>Name" aria-describedby="editWeapons<?=$row['customer_id']?>Help" name="iName" value="<?=$row['customerName']?>"> <!-- customerName -->
+                                                        Phone Number
+                                                        <input type="text" class="form-control" id="editWeapons<?=$row['customer_id']?>Name" aria-describedby="editWeapons<?=$row['customer_id']?>Help" name="iPhone" value="<?=$row['customerPhone']?>"> <!-- customerPhone -->
+                                                        <div id="editWeapons<?=$row['customer_id']?>Help" class="form-text">Enter the customer's name.</div>
+                                                    </div>
+                                                    <input type="hidden" name="iid" value="<?=$row['customer_id']?>">
+                                                    <input type="hidden" name="saveType" value="Edit">
+                                                    <input type="submit" class="btn btn-primary" value="Submit">
+                                                </form>
+                                            </div>
                                         </div>
-                                        <input type="hidden" name="iid" value="<?=$row['customer_id']?>">
-                                        <input type="hidden" name="saveType" value="Edit">
-                                        <input type="submit" class="btn btn-primary" value="Submit">
-                                    </form>
+                                    </div>
                                 </div>
-                            </div>
+                            </td>
+                            <td>
+                                <form method="post" action="">
+                                    <input type="hidden" name="iid" value="<?=$row['customer_id']?>" />
+                                    <input type="hidden" name="saveType" value="Delete">
+                                    <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
+                                </form>
+                            </td>
+                        </tr>
+
+                        <?php
+                        }
+                        } else {
+                        echo "0 results";
+                        }
+                        $conn->close();
+                        ?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
+
+            <br /> <!-- Space Break-->
+
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addWeapons">
+                Add New
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="addWeapons" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addWeaponsLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="addWeaponsLabel">Add Customer Information</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="post" action="">
+                                <div class="mb-3">
+                                    <label for="WeaponsName" class="form-label">Name</label>
+                                    <input type="text" class="form-control" id="WeaponsName" aria-describedby="nameHelp" name="iName"> <!-- input name-->
+                                    <input type="text" class="form-control" id="CustomerPhone" aria-describedby="nameHelp" name="iPhone"> <!-- input phone number-->
+                                    <div id="nameHelp" class="form-text">Enter the Customer's info.</div>
+                                </div>
+                                <input type="hidden" name="saveType" value="Add">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
                         </div>
                     </div>
-                </td>
-                <td>
-                    <form method="post" action="">
-                        <input type="hidden" name="iid" value="<?=$row['customer_id']?>" />
-                        <input type="hidden" name="saveType" value="Delete">
-                        <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
-                    </form>
-                </td>
-            </tr>
-
-            <?php
-            }
-            } else {
-            echo "0 results";
-            }
-            $conn->close();
-            ?>
-
-        </tbody>
-    </table>
-    <br />
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addWeapons">
-        Add New
-    </button>
-
-    <!-- Modal -->
-    <div class="modal fade" id="addWeapons" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addWeaponsLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="addWeaponsLabel">Add Customer Information</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form method="post" action="">
-                        <div class="mb-3">
-                            <label for="WeaponsName" class="form-label">Name</label>
-                            <input type="text" class="form-control" id="WeaponsName" aria-describedby="nameHelp" name="iName"> <!-- input name-->
-                            <input type="text" class="form-control" id="CustomerPhone" aria-describedby="nameHelp" name="iPhone"> <!-- input phone number-->
-                            <div id="nameHelp" class="form-text">Enter the Customer's info.</div>
-                        </div>
-                        <input type="hidden" name="saveType" value="Add">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
                 </div>
             </div>
-        </div>
-    </div>
 
 
 
-    <a class="btn btn-primary" href="index.php" role="button">Home</a>
+            <a class="btn btn-primary" href="index.php" role="button">Home</a>
 
-    <div class="fw-bold ">
-        <!-- footer divs -->
-        <hr />
-        <div>
-            <!-- first row divs in footer-->
-            <div style="width: 400px; float: left;">
-                Privacy Policy
+            <div class="fw-bold ">
+                <!-- footer divs -->
+                <hr />
+                <div>
+                    <!-- first row divs in footer-->
+                    <div style="width: 400px; float: left;">
+                        Privacy Policy
+                    </div>
+
+                    <div style="width:400px; float:left;">
+                        Terms & Conditions
+                    </div>
+
+                    <div style="width: 400px; float: left;">
+                        Do Not Sell or Share My Personal Information
+                    </div>
+
+                </div>
+
+
+
+                <div>
+                    <div style="width: 400px; float: left;">
+                        Cookie Settings
+                    </div>
+
+                    <div style="width: 400px; float: left;">
+                        @2018 - 2022 The Ice Scream Comapny Inc.
+                    </div>
+
+                </div>
+
+
+
             </div>
-
-            <div style="width:400px; float:left;">
-                Terms & Conditions
-            </div>
-
-            <div style="width: 400px; float: left;">
-                Do Not Sell or Share My Personal Information
-            </div>
-
-        </div>
-
-
-
-        <div>
-            <div style="width: 400px; float: left;">
-                Cookie Settings
-            </div>
-
-            <div style="width: 400px; float: left;">
-                @2018 - 2022 The Ice Scream Comapny Inc.
-            </div>
-
-        </div>
-
-
-
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
