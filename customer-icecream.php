3410e4394b940179cd54085175fd5e3f333140b4
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-12-01 03:25:32 -0600

    Update customer-icecream.php

--- customer-icecream.php
+++ customer-icecream.php
@@ -9,6 +9,12 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 
+<script>
+    let d = new Date();
+    alert("Today's date is " + d);
+
+</script>
+
 </head>
 
 <style>
